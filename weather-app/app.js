29e65b5a643b9f508e4c5d5020173d0b565c7aa9
rishabh <rishabhgoyal1803@gmail.com>
2020-09-11 19:08:42 +0530

    dynamic weather based on location

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,22 @@
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-geocode('chandigarh',(error, data) => {
-    if(error) {
-        console.log('Error ',error)
-    } else {
-        forecast(data.latitide, data.longitude, (forecastError, forecastData) => {
-            if(error) {
-                console.log('Error', forecastError);
-            } else {
-                console.log('Its ' + forecastData + ' degrees celcius out there in '+ data.location);
-            }
-        })
-    }
-})
+const cityName = process.argv[2]
+
+if(!cityName) {
+    console.log('Please provide the city name')
+} else {
+    geocode(cityName,(error, data) => {
+        if(error) {
+            console.log('Error ',error)
+        } else {
+            forecast(data.latitide, data.longitude, (forecastError, forecastData) => {
+                if(error) {
+                    console.log('Error', forecastError);
+                } else {
+                    console.log('Its ' + forecastData + ' degrees celcius out there in '+ data.location);
+                }
+            })
+        }
+    })
+}
