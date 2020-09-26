7256dc2f52d02db5e46bc5306c3972f115083e87
rishabh <rishabhgoyal1803@gmail.com>
2020-09-26 11:53:53 +0530

    handle bars intro

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,40 @@
 
 const path = require('path');
 const express = require('express');
+const { read } = require('fs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public/');
 
+// Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public/');
+const viewsPath = path.join(__dirname, "../templates");
+
+// Handlebars engine and custom views
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to server
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: "Weather App",
+        name: "Rishabh Goyal"
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render('about', {
+        title: "About Me",
+        name: "Rishabh Goyal"
+    });
+})
+
+app.get("/help", (req, res) => {
+    res.render('help', {
+        helpMessage: "This is the help message"
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
